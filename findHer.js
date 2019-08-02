3cc69323c7d76055a6a2e1de39912b5068847a40
alligoldstein <52976740+alligoldstein@users.noreply.github.com>
2019-08-02 11:28:13 -0400

    Update findHer.js

--- findHer.js
+++ findHer.js
@@ -1,17 +1,3 @@
-/*carousel();
-function carousel(){
-  var y;
-  var t =document.getElementsByClassname("theSlides");
-  for (y=0; y < t.length; i++) {
-    t[y].style.display = "none";
-  }
-  index++;
-  if (index > t.length) {index=1}
-  x[index-1].style.display = "block";
-  setTimeout(carousel,2000); //change image evry 2 seconds
-}*/
-
-
 function scrollabout(){
   var yay = document.getElementById('about');
   yay.scrollIntoView();

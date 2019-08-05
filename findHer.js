1307a1a2c928a7d9913a13737c5430cebe8adb0b
thenigerianjollof <52325181+thenigerianjollof@users.noreply.github.com>
2019-08-05 09:49:51 -0400

    Add files via upload

---
+++ findHer.js
@@ -0,0 +1,14 @@
+function scrollabout(){
+  var yay = document.getElementById('about');
+  yay.scrollIntoView();
+}
+
+function scrollconsider(){
+  var yay = document.getElementById('forfuture');
+  yay.scrollIntoView();
+}
+
+function scrollpartner(){
+  var yay = document.getElementById('partner');
+  yay.scrollIntoView();
+}

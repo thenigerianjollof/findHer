4cdb986c55058b33398e52effd18fbf569a08b7e
thenigerianjollof <52325181+thenigerianjollof@users.noreply.github.com>
2019-08-05 13:07:39 -0400

    Delete getbutton.py

--- getbutton.py
+++
@@ -1,6 +0,0 @@
-import serial
-arduino = serial.Serial('COM3', 9600, timeout=.1)
-while True:
-	data = str(arduino.readline()[:-2]) #the last bit gets rid of the new-line chars
-	if data == "b'send'":
-		print(data)
